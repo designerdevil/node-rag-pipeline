f243e0a501f39b4977570cd0ff1c14a0e070d1a0
Gaurav Mall <dev.gauravmall@gmail.com>
2024-08-11 23:10:01 +0530

    removing web search package

--- util/retrievalHandler.js
+++ util/retrievalHandler.js
@@ -1,14 +1,11 @@
 import { FaissStore } from "@langchain/community/vectorstores/faiss";
 import embeddings from "./embeddings.js";
-import webSearch from "./web-search.js";
 
 const retrieverHandler = async (query, fromWeb=false) => {
 	const vectorStore = await FaissStore.load(`./embeds`, embeddings);
 	const records = await vectorStore.similaritySearch(query);
-
-	const infoFromWeb = fromWeb ? await webSearch(query) : [];
 	
-	const context = [...records.map((item) => item.pageContent), JSON.stringify(infoFromWeb)];
+	const context = [...records.map((item) => item.pageContent)];
 	return context
 }
 
